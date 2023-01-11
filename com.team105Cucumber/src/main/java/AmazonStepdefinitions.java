7c25de5121a363b07c2e3e3c97fc2d0f906673af
hasansacikara <sacikara33@gmail.com>
2023-01-11 12:11:55 +0300

    19.gün sonu cucumber geçildi

---
+++ com.team105Cucumber/src/main/java/AmazonStepdefinitions.java
@@ -0,0 +1,28 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import java.sql.Driver;
+
+public class AmazonStepdefinitions {
+
+    @Given("kullanici amazon anasayfaya gider")
+    public void kullanici_amazon_anasayfaya_gider() {
+
+    }
+
+    @Then("amazon arama kutusuna Nutella yazip aratir")
+    public void amazon_arama_kutusuna_nutella_yazip_aratir() {
+
+    }
+
+    @Then("arama sunuclarinin Nutella icerdigini test eder")
+    public void arama_sunuclarinin_nutella_icerdigini_test_eder() {
+
+    }
+
+    @Then("sayfayi kapatir")
+    public void sayfayi_kapatir() {
+
+    }
+
+}
